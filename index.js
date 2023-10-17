0c0392f5aa4c6bdfe559bafa0fee6858f639312e
TathusT <138441084+TathusT@users.noreply.github.com>
2023-10-17 21:12:53 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const line = require("@line/bot-sdk");
+const express = require("express");
+const axios = require("axios").default;
+const bodyParser = require("body-parser");
+var cors = require("cors");
+
+var corsOption = {
+  origin: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+const app = express();
+app.use(cors(corsOption));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const lineConfig = {
+  channelAccessToken:
+    "lgOJd3BzZij3BujNXREFQZ9VrHYRnQoaBa0irWnzhNnkJTYXW5yDYomDYsbVqDZIFZ1HwnwcVsuxCFqbf/Fd7UxjZ0aHchxWGAx8/cbcDF5q82f+k5gqM3ocQSQd00aa+/IEo/kV7NLIm+QZX6VXbwdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "0b89248e5ea046b327297535b7ec3af1",
+};
+
+app.post("/login", async (req, res) => {
+  let profile = req.body.lineProfile;
+  let uid = profile.userId;
+  let urlRequest = `https://api.line.me/v2/bot/user/${uid}/richmenu/richmenu-b97490662563823d58aa30a4f75aed83`;
+  console.log(urlRequest);
+  await axios.request({
+    method: "POST",
+    url: `${urlRequest}`,
+    headers: {
+      Authorization: `Bearer ` + lineConfig.channelAccessToken,
+    },
+  });
+  res.json(profile);
+});
+
+app.post("/logout", async (req, res) => {
+    let profile = req.body.lineProfile;
+    let uid = profile.userId;
+    let urlRequest = `https://api.line.me/v2/bot/user/${uid}/richmenu/richmenu-cfb50427a3796fbb429957ec8bda2686`;
+    console.log(urlRequest);
+    await axios.request({
+      method: "POST",
+      url: `${urlRequest}`,
+      headers: {
+        Authorization: `Bearer ` + lineConfig.channelAccessToken,
+      },
+    }).catch((error) =>{
+        console.error("Error calling LINE API:", error.response.data);
+    });
+    res.json(profile);
+  });
+
+app.listen(4000, function () {
+  console.log("listening on port", this.address().port);
+});
